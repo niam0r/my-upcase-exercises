8f15aa7c14489a935ff84b85564a2f65b3712dec
Romain niam0r <niamor@mm.st>
2018-12-12 17:02:14 +0000

    implement "user creates person" test with capybara

--- testing-fundamentals-write-an-integration-test/spec/features/user_creates_person_spec.rb
+++ testing-fundamentals-write-an-integration-test/spec/features/user_creates_person_spec.rb
@@ -2,6 +2,12 @@ require "rails_helper"
 
 feature "user creates person" do
   scenario "with valid data" do
-    pending "Use Capybara to create a new person"
+    visit new_person_url
+
+    fill_in "First name", with: "John"
+    click_button "Create Person"
+
+    expect(page).to have_content("Person created.")
+    expect(page).to have_content("John")
   end
 end
