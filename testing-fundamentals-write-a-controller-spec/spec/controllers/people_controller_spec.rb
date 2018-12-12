1a8cfbe1f036c796439cae1133f6a85d87909d2a
Romain niam0r <niamor@mm.st>
2018-12-12 18:06:22 +0000

    implement test without stubbing

--- testing-fundamentals-write-a-controller-spec/spec/controllers/people_controller_spec.rb
+++ testing-fundamentals-write-a-controller-spec/spec/controllers/people_controller_spec.rb
@@ -4,13 +4,15 @@ describe PeopleController do
   describe "#create" do
     context "when person is valid" do
       it "redirects to #show" do
-        pending "create this test"
+        post :create, person: { first_name: "John" }
+        expect(response).to redirect_to person_path(assigns(:person))
       end
     end
 
     context "when person is invalid" do
       it "renders the 'new' template" do
-        pending "create this test"
+        post :create, person: { first_name: "" }
+        expect(response).to render_template(:new)
       end
     end
   end
