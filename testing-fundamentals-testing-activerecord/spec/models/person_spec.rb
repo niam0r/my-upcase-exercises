fef2d5bfdb67f26aa3a67baff8bb1152b43f23e8
Romain niam0r <niamor@mm.st>
2018-12-12 16:15:57 +0000

    implement "is invalid without a first_name"

--- testing-fundamentals-testing-activerecord/spec/models/person_spec.rb
+++ testing-fundamentals-testing-activerecord/spec/models/person_spec.rb
@@ -1,9 +1,10 @@
 require "spec_helper"
 
 describe Person do
-  it "requires a name to be valid" do
-    # Write your spec here.
-    # You can delete the pending line.
-    pending "Not written yet"
+  it "is invalid without a first_name" do
+    person = Person.new(first_name: nil)
+
+
+    expect(person).to be_invalid
   end
 end
